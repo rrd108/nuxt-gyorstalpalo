7cb9f9ce33b930de2edcd034763997bb643ea144
rrd <rrd@1108.cc>
2022-05-28 11:03:13 +0200

    index and about page added

--- app.vue
+++ app.vue
@@ -1,5 +1,25 @@
+<script setup></script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <nav>
+      <ul>
+        <li><NuxtLink to="/">Shop</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+      </ul>
+    </nav>
+    <main>
+      <NuxtPage />
+    </main>
   </div>
 </template>
+
+<style scoped>
+  div {
+    display: grid;
+    grid-template-columns: 1fr 5fr;
+    max-width: 56rem;
+    margin: 0 auto;
+    gap: 1em;
+  }
+</style>
